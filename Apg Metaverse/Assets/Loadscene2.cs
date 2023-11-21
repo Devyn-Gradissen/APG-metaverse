2d9821ea7763765da6ea230d1952224451bcbbc7
SjoerdGroenheide <91879864+SjoerdGroenheide@users.noreply.github.com>
2023-11-21 13:03:29 +0100

    instellingen en keybinds aangepast

---
+++ Apg Metaverse/Assets/Loadscene2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Loadscene2 : MonoBehaviour
+{
+public void LoadSceneByName()
+{
+    SceneManager.LoadScene("Instellingen");
+}
+}
+
