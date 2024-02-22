6f957c135bc0e4641a86e72d9420c3fa13bc270d
Devyngradissen <wolvenzijncool87@gmail.com>
2024-02-22 14:50:59 +0100

    data fixed

--- Apg Metaverse/Assets/chat/dataloader.cs
+++ Apg Metaverse/Assets/chat/dataloader.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking; // Import UnityWebRequest
 
 public class dataloader : MonoBehaviour
 {
@@ -7,14 +8,21 @@ public class dataloader : MonoBehaviour
 
     IEnumerator Start()
     {
-        WWW ChatData = new WWW("http://localhost/school%20leerjaar%203/chatdata/ChatData.php"); //connectie met de data
-        yield return ChatData; // wacht totdat data is gedownload
+        UnityWebRequest ChatDataRequest = UnityWebRequest.Get("http://localhost/school%20leerjaar%203/chatdata/ChatData.php"); //connectie met de data
+        yield return ChatDataRequest.SendWebRequest(); // wacht totdat data is gedownload
         
-        string ChatDataString = ChatData.text; //maak de data klaar om te zien
-        print(ChatDataString); // laat de data zien
-        
-        chat = ChatDataString.Split(';');
-        print(GetDataValue(chat[0], "chat")); // Replace "your_index_here" with the actual index you want to retrieve
+        if (ChatDataRequest.result == UnityWebRequest.Result.Success)
+        {
+            string ChatDataString = ChatDataRequest.downloadHandler.text; //maak de data klaar om te zien
+            Debug.Log(ChatDataString); // laat de data zien
+            
+            chat = ChatDataString.Split(';');
+            Debug.Log(GetDataValue(chat[0], "chat")); // Replace "your_index_here" with the actual index you want to retrieve
+        }
+        else
+        {
+            Debug.LogError("Error downloading chat data: " + ChatDataRequest.error);
+        }
     }
 
     string GetDataValue(string data, string index)
