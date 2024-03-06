8627861413421ae8df02ab4353ec6d1524ea4bae
Devyngradissen <wolvenzijncool87@gmail.com>
2024-03-06 12:40:29 +0100

    vr en chat movement fix
    vr is werkend in the character design nu en de bug dat je moved als je typed is ook gefixt alleen de enter bug is nog niet gefixt

--- Apg Metaverse/Assets/GameManager.cs
+++ Apg Metaverse/Assets/GameManager.cs
@@ -5,71 +5,53 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public string username;
+    public int maxMessages = 100;
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
+    public Color playerMessage, info;
 
-     ArrayList credentials;  
-     
-    //public Text welcomeText;
-
-    public string username; // User identifier (deze houd je gebruikersnaam vast )
-
-    // Manages message history size
-    public int maxMessages = 100; // Message history limit (Determines the maximum number of messages in the chat history)
-
-    // UI elements
-    public GameObject chatPanel, textObject; // Visual chat elements (References to the chat panel and text objects in the Unity UI)
-    public InputField chatBox; // Input field for chat messages (The input field where the player enters messages)
-
-    public Color playerMessage, info; // Message color options (Colors for different types of messages)
-
-    // Private list to store messages
     [SerializeField]
-    List<Message> messageList = new List<Message>(); // Message storage (A list to store chat messages)
+    List<Message> messageList = new List<Message>();
 
-    // Start is called before the first frame update
+    public PlayerController playerController; // Reference to the PlayerController script
+
     void Start()
     {
-   
-        //welcomeText.text = "Welcome, " + username + "!";
+        playerController.SetGameManager(this); // Set reference to GameManager in PlayerController
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (chatBox.text != "")
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                // Send player's message and clear input
                 SendMessageToChat(username + ": " + chatBox.text, Message.MessageType.playerMessage);
                 chatBox.text = "";
+
+                // Toggle player movement
+                playerController.ToggleMovement(false);
             }
         }
         else
         {
             if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
-                chatBox.ActivateInputField(); // Activate chat input (When no text is entered, pressing Enter activates the chat input field)
-        }
+                chatBox.ActivateInputField(); // Activate chat input
 
-       // if (!chatBox.isFocused)
-       // {
-       //     if (Input.GetKeyDown(KeyCode.Space))
-       //         SendMessageToChat("Button Test", Message.MessageType.info); // Send test message to chatbox (Sending a test message when the chat is not focused)
-         //   Debug.Log("Space"); // Log "Space" to the console
-// }
+            // Ensure movement is enabled when chat is not focused
+            playerController.ToggleMovement(true);
+        }
     }
 
-    // Send a message to the chat
-    public void SendMessageToChat(string text, Message.MessageType messageType)
+    void SendMessageToChat(string text, Message.MessageType messageType)
     {
-        // Ensure message history doesn't exceed the maximum limit for the chatbox
         if (messageList.Count >= maxMessages)
         {
-            // Remove the oldest message from the chatbox
             Destroy(messageList[0].textObject.gameObject);
             messageList.Remove(messageList[0]);
         }
 
-        // Prepare and display the new message in the chatbox
         Message newMessage = new Message();
 
         newMessage.text = text;
@@ -79,23 +61,23 @@ public class GameManager : MonoBehaviour
         newMessage.textObject = newText.GetComponent<Text>();
 
         newMessage.textObject.text = newMessage.text;
-        newMessage.textObject.color = MessageTypeColor(messageType); // Set message color
+        newMessage.textObject.color = MessageTypeColor(messageType);
 
         messageList.Add(newMessage);
     }
 
     Color MessageTypeColor(Message.MessageType messageType)
     {
-        Color color = info; // Default message color
+        Color color = info;
 
         switch (messageType)
         {
             case Message.MessageType.playerMessage:
-                color = playerMessage; // Set player's message color
+                color = playerMessage;
                 break;
         }
 
-        return color; // Return the selected color
+        return color;
     }
 }
 
@@ -108,7 +90,7 @@ public class Message
 
     public enum MessageType
     {
-        playerMessage, // Player's message type
-        info // General information message type
+        playerMessage,
+        info
     }
 }
