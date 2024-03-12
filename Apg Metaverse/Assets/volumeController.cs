43726e3a2005a12cbb36602bd773067de7a85bd9
SjoerdGroenheide <507041@vistacollege.nl>
2024-03-12 10:53:54 +0100

    instellingen doen het in vr

--- Apg Metaverse/Assets/volumeController.cs
+++ Apg Metaverse/Assets/volumeController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UI.Audio;
+
 
 public class volumeController : MonoBehaviour
 {
