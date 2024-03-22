0fb80dd7545418d020aa1ef10c4261c20d53926e
SjoerdGroenheide <507041@vistacollege.nl>
2024-03-22 13:46:33 +0100

    slider gaat naar database werkt 100% alleen nog push to talk naar database

--- Apg Metaverse/Assets/SaveSliderToDatabase.cs
+++ Apg Metaverse/Assets/SaveSliderToDatabase.cs
@@ -4,39 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class SaveSliderValue : MonoBehaviour
-{
+public class SaveSliderValue : MonoBehaviour 
+{ 
+    // alle sliders en de link naar de script naar de database benoemen en koppelen in unity
     public Slider Helderheid;
     public Slider Volume;
     public Slider Kleurenblind;
     public string phpScriptURL;
 
-    public void SaveSliderToDatabase()
+    public void SaveSliderToDatabase() //zorgen dat het script gestart wordt
     {
         StartCoroutine(SendSliderValue());
     }
 
     IEnumerator SendSliderValue()
     {
-        // Create form
+        Debug.Log("HelderheidValue: " + Helderheid.value.ToString());
+        Debug.Log("VolumeValue: " + Volume.value.ToString());
+        Debug.Log("KleurenblindValue: " + Kleurenblind.value.ToString());
+
+        // maak het form, zodat het naar de database gestuurt kan worden
         WWWForm form = new WWWForm();
         form.AddField("HelderheidValue", Helderheid.value.ToString());
         form.AddField("VolumeValue", Volume.value.ToString());
         form.AddField("KleurenblindValue", Kleurenblind.value.ToString());
       
 
-        // Send data to PHP script
+        // stuur data naar php script gelinkt bovenaan
         using (UnityWebRequest www = UnityWebRequest.Post(phpScriptURL, form))
         {
             yield return www.SendWebRequest();
 
             if (www.result != UnityWebRequest.Result.Success)
             {
-                Debug.LogError("Failed to send slider value to database: " + www.error);
+                Debug.LogError("Failed to send slider value to database: " + www.error); // als er fouten zijn worden die getoont
             }
             else
             {
-                Debug.Log("Slider value saved successfully");
+                Debug.Log("Slider value saved successfully"); // als de slider het goed heeft opgeslagen dan komt dit in unity te staan
             }
         }
     }
--- Apg Metaverse/Assets/SliderManager.cs
+++ Apg Metaverse/Assets/SliderManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class SliderManager : MonoBehaviour
 {
+    // zorgen dat je de sliders kan koppelen in unity
     public Slider Helderheid;
     public Slider Volume;
     public Slider Kleurenblind;
@@ -18,7 +19,8 @@ public class SliderManager : MonoBehaviour
     IEnumerator SendSliderValue()
     {
         // Replace 'your_php_script_url' with the URL to your PHP script
-        string url = "http://localhost/school%20leerjaar%203/slider/save_slider.php";
+        string url = "http://localhost/school%20leerjaar%203/slider/save_slider.php"; // link naar het php script
+        // form aanmaken voor de database
         WWWForm form = new WWWForm();
         form.AddField("HelderheidValue", (int)Helderheid.value);
         form.AddField("VolumeValue", (int)Volume.value);
