337a69dc755a7be67e4dc7f46ff4ca8409caad83
Devyngradissen <wolvenzijncool87@gmail.com>
2024-06-10 09:16:41 +0200

    tutorial af

---
+++ Apg Metaverse/Assets/Scenes/VRButton.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class VRButton : MonoBehaviour
+{
+    public int sceneIndex; // Index of the scene to load when the button is pressed
+
+    // You can add more functionality here as needed
+}
