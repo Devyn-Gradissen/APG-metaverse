5b16fc528844b8deb091d6d75ef5de45fe7834e7
Tim Smeets <tim.smeets.2004@gmail.com>
2024-06-25 15:55:11 +0200

    Update movementtest.cs

--- Apg Metaverse/Assets/movementtest.cs
+++ Apg Metaverse/Assets/movementtest.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public float moveSpeed = 5f;
+    public float sprintSpeed = 12f;
     public float mouseSensitivity = 2f; // Sensitivity of mouse movement
     public Camera playerCamera; // Assign the camera in the Unity editor
 
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
 
     private GameManager gameManager; // Reference to the GameManager script
     private bool isMovementEnabled = true; // Flag to track if movement is enabled
+    private bool sprinting = false;
 
     void Start()
     {
@@ -96,6 +98,8 @@ public class PlayerController : MonoBehaviour
                     // Set the animator parameter IsMoving to true so that Unity starts the animation when you move
                     animator.SetBool("IsMoving", true);
                 }
+
+                sprinting = Input.GetButton("Sprint");
 
                 // Detect collisions and adjust movement direction
                 AdjustMovementDirection(ref movement);
